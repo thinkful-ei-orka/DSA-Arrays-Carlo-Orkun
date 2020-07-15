824f5758a58b9e39ff7d7ee3a2578082b3ef64d5
Orkun Ozekin <orkunozekin@thinkful.com>
2020-07-15 13:27:03 -0500

    initial commit

---
+++ class.js
@@ -0,0 +1,16 @@
+// 1. Implement an Array class from scratch.
+// Walk through each step of implementing an array. Don't rush through this by copying and pasting the code samples. After you've walked through it and you understand the code of the Array class, hide the sample code and try writing the Array class from scratch using the memory module here for allocating memory.
+
+
+
+const Memory = require('./memory-module');
+
+class Array {
+    constructor() {
+        this.length = 0;
+        // this.ptr = memory.allocate(this.length)
+        this.data = {};
+    }
+
+    
+}
